dc43d805e73b0c2027a9333d14f4e6d905b1aafb
Harsh Raj <harsh.techilia@gmail.com>
2024-01-15 20:07:26 +0530

    getting started

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="h-dvh w-full bg-slate-800 text-slate-50">
+        <h1 className="text-5xl">Test</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
