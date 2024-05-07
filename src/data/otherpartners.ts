81fd133cda5ad0cde5aa61802dda7c08366891ac
PranjaliChouhan <pranjalichouhan4@gmail.com>
2024-05-07 19:40:50 +0530

    homepage

---
+++ src/data/otherpartners.ts
@@ -0,0 +1,100 @@
+import spookyswap from '@/assets/icons/spookyswap.svg'
+ 
+ export const otherpartners = [
+    {
+      name: 'Spookyswap',
+      icon: spookyswap,
+    },
+    {
+      name: 'cointelegraph',
+      icon: spookyswap,
+    },
+    {
+      name: 'devoted',
+      icon: spookyswap,
+    },
+    {
+      name: 'whimsy games',
+      icon: spookyswap,
+    },
+    {
+      name: 'safepal',
+      icon: spookyswap,
+    },
+    {
+      name: 'okse',
+      icon: spookyswap,
+    },
+    {
+      name: 'hafen',
+      icon: spookyswap,
+    },
+    {
+      name: 'wingswap',
+      icon: spookyswap,
+    },
+    {
+      name: 'balena',
+      icon: spookyswap,
+    },
+    {
+      name: 'oig',
+      icon: spookyswap,
+    },
+    {
+      name: 'dct capital',
+      icon: spookyswap,
+    },
+    {
+      name: 'master ventures',
+      icon: spookyswap,
+    },
+    {
+      name: 'seedthrift',
+      icon: spookyswap,
+    },
+    {
+      name: 'ex',
+      icon: spookyswap,
+    },
+    {
+      name: 'fantomstarter',
+      icon: spookyswap,
+    },
+    {
+      name: 'pack capital',
+      icon: spookyswap,
+    },
+    {
+      name: 'deltahub',
+      icon: spookyswap,
+    },
+    {
+      name: 'gate.io',
+      icon: spookyswap,
+    },
+    {
+      name: 'chainplay',
+      icon: spookyswap,
+    },
+    {
+      name: 'castrum',
+      icon: spookyswap,
+    },
+    {
+      name: 'gaming',
+      icon: spookyswap,
+    },
+    {
+      name: 'zr',
+      icon: spookyswap,
+    },
+    {
+      name: 'bull',
+      icon: spookyswap,
+    },
+    {
+      name: 'flame',
+      icon: spookyswap,
+    },
+  ]
---
+++ src/data/strengths.ts
@@ -0,0 +1,25 @@
+import strength1 from '@/assets/images/strength1.png'
+import strength2 from '@/assets/images/strength2.png'
+import strength3 from '@/assets/images/strength3.png'
+
+
+
+const strengths = [
+    {
+      title: 'Diversified Revenue Streams:',
+      desc: 'We understand the cyclical nature of markets and proactively diversify our revenue channels for stability and growth.',
+      image: strength1,
+    },
+  
+    {
+      title: 'Prudent Financial Management:',
+      desc: 'Our commitment to fiscal responsibility ensures lean operations and strategic expenditures for a solid foundation.',
+      image: strength2,
+    },
+    {
+      title: 'Adaptive Game Development Strategy:',
+      desc: 'We adapt to player behavior shifts and economic trends to ensure consistent engagement.',
+      image: strength3,
+    },
+  ]
+  export default strengths
