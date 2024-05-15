872311978c2302c67267b30a09935a09a6302926
PranjaliChouhan <pranjalichouhan4@gmail.com>
2024-05-15 15:03:49 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepagemcrt from './pages/Homepagemcrt'
 import Homepagegames from './pages/HomePageGames'
 import Support from './pages/Support';
+import GamePatch from './pages/GamePatch';
 // import Homepagemagicrunner from './pages/HomePageMagicRunner';
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
       {/* <Route path="/magicrunner" element={<Homepagemagicrunner />}> */}
       </Route>
       <Route path="/faq" element={<Support />}>
+      </Route> 
+      <Route path="/patch" element={<GamePatch />}>
       </Route> 
     </Routes>
   </BrowserRouter>
