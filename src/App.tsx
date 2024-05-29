e2f2a0cfbd4a7913808eeb28ddda884df735283c
PranjaliChouhan <pranjalichouhan4@gmail.com>
2024-05-29 14:20:48 +0530

    updated mcrt

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Homepagemcrt from './pages/Homepagemcrt'
 import Homepagegames from './pages/HomePageGames'
-import Support from './pages/Support';
-import GamePatch from './pages/GamePatch';
-import Homepagemagicrunner from './pages/HomePageMagicRunner';
-
+import Support from './pages/Support'
+import GamePatch from './pages/GamePatch'
+import Homepagemagicrunner from './pages/HomePageMagicRunner'
+import Homepagemagic8ball from './pages/HomePageMagic8Ball'
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepagegames />}>
-      </Route>
-      <Route path="/mcrt" element={<Homepagemcrt />}>
-       </Route> 
-       <Route path="/magicrunner" element={<Homepagemagicrunner />}>
-      </Route>
-      <Route path="/faq" element={<Support />}>
-      </Route> 
-      <Route path="/patch" element={<GamePatch />}>
-      </Route> 
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepagegames />}></Route>
+        <Route path="/mcrt" element={<Homepagemcrt />}></Route>
+        <Route path="/magicrunner" element={<Homepagemagicrunner />}></Route>
+        <Route path="/magic8ball" element={<Homepagemagic8ball />}></Route>
+
+        <Route path="/faq" element={<Support />}></Route>
+        <Route path="/patch" element={<GamePatch />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/data/otherpartners.ts
+++ src/data/otherpartners.ts
@@ -1,123 +1,130 @@
-import cointelegraph from '@/assets/icons/cointelegraph.svg'
+import unix from '@/assets/icons/Unix.svg'
+import glip from '@/assets/icons/glip.svg'
+import magic from '@/assets/icons/magic.svg'
+import elixir from '@/assets/icons/elixir.svg'
+import salad from '@/assets/icons/salad.svg'
+import gameswift from '@/assets/icons/gameswift.svg'
+import portal from '@/assets/icons/portal.svg'
+import stepn from '@/assets/icons/stepn.svg'
+import ovio from '@/assets/icons/ovioLogo 1.svg'
+import nftb from '@/assets/icons/nftb.svg'
 import devoted from '@/assets/icons/devoted.svg'
-import whimsy from '@/assets/icons/whimsy.svg'
-import safepal from '@/assets/icons/safepal.svg'
-import okse from '@/assets/icons/okse.svg'
-import hafen from '@/assets/icons/hafen.svg'
-import wingswap from '@/assets/icons/wingswap_black.svg'
-import balena from '@/assets/icons/balena.svg'
-import oig from '@/assets/icons/oig.svg'
-import gateio from '@/assets/icons/gateio_black.svg'
+import whimsy from '@/assets/icons/Whimsy Games LOGO.svg'
 import chainplay from '@/assets/icons/chainplay.svg'
-import castrum from '@/assets/icons/castrum.svg'
-import gaming from '@/assets/icons/gaming.svg'
-import zr from '@/assets/icons/zr.svg'
-import bull from '@/assets/icons/bull.svg'
+import soquest from '@/assets/icons/soquest.svg'
+import cointelegraph from '@/assets/icons/Coin telegram.svg'
+import safepal from '@/assets/icons/safepal.svg'
+import owned from '@/assets/icons/owned.svg'
+
+import gam3s from '@/assets/icons/GAM3S.svg'
 import flame from '@/assets/icons/flame.svg'
-import ex from '@/assets/icons/ex.svg'
-import dct from '@/assets/icons/dct.svg'
-import master from '@/assets/icons/master_venture.svg'
-import seedthrift from '@/assets/icons/seed_thrift.svg'
-import fantom from '@/assets/icons/fantomstarter.svg'
-import deltahub from '@/assets/icons/deltahub.svg'
-import pack from '@/assets/icons/pack_capital.svg'
-import spooky from '@/assets/icons/spookyswap_black.svg'
+import wombat from '@/assets/icons/wombat.svg'
+import creo from '@/assets/icons/creoengine.svg'
+import carv from '@/assets/icons/carv.svg'
+import earn from '@/assets/icons/earn.svg'
+import gate from '@/assets/icons/gate.svg'
+import castrum from '@/assets/icons/castrum.svg'
+
  
  export const otherpartners = [
     {
-      name: 'Spookyswap',
-      icon: spooky,
+      name: 'UNIX',
+      icon: unix,
     },
     {
-      name: 'cointelegraph',
-      icon: cointelegraph,
+      name: 'GLIP.GG',
+      icon: glip,
     },
     {
-      name: 'devoted',
+      name: 'Magic Square',
+      icon: magic,
+    },
+    {
+      name: 'Elixir',
+      icon: elixir,
+    },
+    {
+      name: 'Salad',
+      icon: salad,
+    },
+    {
+      name: 'GameSwift',
+      icon: gameswift,
+    },
+    {
+      name: 'Portal',
+      icon: portal,
+    },
+    {
+      name: 'STEPN',
+      icon: stepn,
+    },
+    {
+      name: 'OviO',
+      icon: ovio,
+    },
+    {
+      name: 'NFTb',
+      icon: nftb,
+    },
+    {
+      name: 'Devoted Studios',
       icon: devoted,
     },
     {
-      name: 'whimsy games',
+      name: 'Whimsy Games',
       icon: whimsy,
     },
     {
-      name: 'safepal',
-      icon: safepal,
-    },
-    {
-      name: 'okse',
-      icon: okse,
-    },
-    {
-      name: 'hafen',
-      icon: hafen,
-    },
-    {
-      name: 'wingswap',
-      icon: wingswap,
-    },
-    {
-      name: 'balena',
-      icon: balena,
-    },
-    {
-      name: 'oig',
-      icon: oig,
-    },
-    {
-      name: 'dct capital',
-      icon: dct,
-    },
-    {
-      name: 'master ventures',
-      icon: master,
-    },
-    {
-      name: 'seedthrift',
-      icon: seedthrift,
-    },
-    {
-      name: 'ex',
-      icon: ex,
-    },
-    {
-      name: 'fantomstarter',
-      icon: fantom,
-    },
-    {
-      name: 'pack capital',
-      icon: pack,
-    },
-    {
-      name: 'deltahub',
-      icon: deltahub,
-    },
-    {
-      name: 'gate.io',
-      icon: gateio,
-    },
-    {
-      name: 'chainplay',
+      name: 'ChainPlay',
       icon: chainplay,
     },
     {
-      name: 'castrum',
-      icon: castrum,
+      name: 'SoQuest',
+      icon: soquest,
     },
     {
-      name: 'gaming',
-      icon: gaming,
+      name: 'The Cointelegraph',
+      icon: cointelegraph,
     },
     {
-      name: 'zr',
-      icon: zr,
+      name: 'SafePal',
+      icon: safepal,
     },
     {
-      name: 'bull',
-      icon: bull,
+      name: 'Owned',
+      icon: owned,
     },
     {
-      name: 'flame',
+      name: 'GAM3S.GG',
+      icon: gam3s,
+    },
+    {
+      name: 'FlameStore',
       icon: flame,
+    },
+    {
+      name: 'Wombat',
+      icon: wombat,
+    },
+    {
+      name: 'Creo Engine',
+      icon: creo,
+    },
+    {
+      name: 'CARV',
+      icon: carv,
+    },
+    {
+      name: 'Earn Alliance',
+      icon: earn,
+    },
+    {
+      name: 'Gate IO',
+      icon: gate,
+    },
+    {
+      name: 'Castrum',
+      icon: castrum,
     },
   ]
