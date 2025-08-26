7485f4cc5520ad50a63da66646f83a47c343c31b
James <james@MacBook-Air.local>
2025-08-26 19:03:06 +0700

    Fix JSX structure: correctly close lobby grid container

--- src/components/HomePageCard.tsx
+++ src/components/HomePageCard.tsx
@@ -510,6 +510,7 @@ export default function MagicraftDownload() {
             </motion.div>
           )}
         </AnimatePresence>
+      </div>
       {/* End Crypto Lobby Cards grid */}
       </div>
     </div>
