ef5b83638813025bbd72bb3dd6b8af0aec400208
James <james@MacBook-Air.local>
2026-07-19 15:59:48 +0700

    Update MagicCraft Sanity Studio dependencies

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -3,11 +3,11 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'basag4bs',
-    dataset: 'blogs'
+    dataset: 'blogs',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  deployment: {autoUpdates: true},
 })
